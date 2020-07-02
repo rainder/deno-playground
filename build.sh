c43a5c37e3370d52d7d0a9e28cad05b0eedb33c0
Andrius Skerla <andy@splytech.io>
2020-07-02 14:01:58 +0100

    bundle

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-mkdir -p build
-
 FILES=*.ts
 for f in $FILES
 do
   echo "Processing $f file..."
-  deno bundle --unstable $f build/${f//ts/js}
+  deno bundle --unstable $f ${f//ts/js}
 done
